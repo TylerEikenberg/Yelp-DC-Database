445799b751f51bc79fc5408eb758ad0ca74ae9f4
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-18 20:21:31 -0500

    updated bash script to get only the 100 best rated restaurants

--- getData.sh
+++ getData.sh
@@ -2,6 +2,7 @@
 
 rm -rf ./db/data/* && 
 touch ./db/data/places && 
-curl -X GET 'https://api.yelp.com/v3/businesses/search?term=restaurants&location=Washington,DC' -H 'Authorization: Bearer fpLbcBy1thaWbp1z6_ECza87BCAiTqFrvXuLcu4mI4nJ2bCGKgzvkl5f-L7YRQdngbmKFa7wHzcqgshsJCqHkMEbz37TDNwXI8AuU6i6u1XLuEIIyc1OeqraJejSXXYx' -H 'Cache-Control: no-cache' > ./db/data/places.json
+curl -X GET 'https://api.yelp.com/v3/businesses/search?term=restaurants&location=Washington,DC&limit=50&offset=0&sort_by=rating' -H 'Authorization: Bearer fpLbcBy1thaWbp1z6_ECza87BCAiTqFrvXuLcu4mI4nJ2bCGKgzvkl5f-L7YRQdngbmKFa7wHzcqgshsJCqHkMEbz37TDNwXI8AuU6i6u1XLuEIIyc1OeqraJejSXXYx' -H 'Cache-Control: no-cache' > ./db/data/places.json
+curl -X GET 'https://api.yelp.com/v3/businesses/search?term=restaurants&location=Washington,DC&limit=50&offset=50&sort_by=rating' -H 'Authorization: Bearer fpLbcBy1thaWbp1z6_ECza87BCAiTqFrvXuLcu4mI4nJ2bCGKgzvkl5f-L7YRQdngbmKFa7wHzcqgshsJCqHkMEbz37TDNwXI8AuU6i6u1XLuEIIyc1OeqraJejSXXYx' -H 'Cache-Control: no-cache' >> ./db/data/places.json
 
 
